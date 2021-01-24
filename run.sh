fb1b3497d7a51a40c8114246da575f56fcd2b857
alxest <youngju.song@sf.snu.ac.kr>
2021-01-25 00:20:30 +0900

    WIP

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-pdflatex -shell-escape main && bibtex main && pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
+#pdflatex -shell-escape main && bibtex main && pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
+pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
