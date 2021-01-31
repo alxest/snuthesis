39783a8319ad79fadba1e74ea1e981d6bdc0a7ab
alxest <youngju.song@sf.snu.ac.kr>
2021-02-01 01:31:53 +0900

    Tweak

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #pdflatex -shell-escape main && bibtex main && pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
-pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
+bibtex main && pdflatex -shell-escape main && (xdg-open main.pdf || open main.pdf)
